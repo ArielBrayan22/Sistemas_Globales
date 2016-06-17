de2940edfcb7e7317da65a966a2c0c8af588bc5c
igroxs <igorbbcc@gmail.com>
2016-06-17 06:13:27 -0400

    Integracion de materializecss y jquery

--- index.php
+++ index.php
@@ -2,9 +2,34 @@
 <head>
 	<title>Sistema de Planes Globales</title>
 	<link rel="stylesheet" type="text/css" href="Style.css">
+	
+	<!--Importacion de Iconos y fuentes-->
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Importacion de materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+    <!--Permitir al Navegador que la web es optimizada para moviles-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
-	<header><center><h2 id="titulo_Principal">Sistema de Planes Globales y Programas Analiticos</h2></center>
+	 <!--Importacion de jQuery y materialize.js y scripts iniciadores-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-3.0.0.min.js"></script>
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+      <script>
+      	$(document).ready(function(){
+      		$('.slider').slider({full_width: true});
+    	});
+      </script>
+
+      <nav>
+  		  <div class="nav-wrapper #e53935 red darken-1">
+      		<a href="" class="brand-logo">SISTEMA DE PLANES GLOBALES Y PROGRAMAS ANALITICOS</a>
+      		  <ul class="right hide-on-med-and-down">
+        	    <li><a href="sass.html"><i class="material-icons right">live_help</i>Ayuda</a></li>
+             	<li><a href="badges.html"><i class="material-icons right">settings_phone</i>Contactanos</a></li>
+      		  </ul>
+    	  </div>
+  	  </nav>
   <hr></hr>
   </header>
 	
@@ -38,37 +63,60 @@
 	</aside>
 
 <article id="cuerpo">
-    <center><img src="imagen.JPG"></center>
+    <div class="slider">
+	    <ul class="slides">
+	      <li>
+	        <img src="imagenes/slider1.jpg"> <!-- random image -->
+	        <div class="caption center-align">
+	          <h4>UNIVERSIDAD MAYOR DE SAN SIMON</h4>
+	          <h5 class="light white-text text-lighten-3">Facultad de Ciencias y Tecnología</h5>
+	        </div>
+	      </li>
+	      <li>
+	        <img src="imagenes/slider2.jpg"> <!-- random image -->
+	        <div class="caption left-align">
+	          <h3>Construyendo el Futuro</h3>
+	          <h5 class="light white-text text-lighten-3">Desde 1832</h5>
+	        </div>
+	      </li>
+	      <li>
+	        <img src="imagenes/slider3.jpg"> <!-- random image -->
+	        <div class="caption right-align">
+	          <h4>Universidad Mayor de San Simón</h4>
+	          <h5 class="light grey-text text-lighten-3">Desde 1832</h5>
+	        </div>
+	      </li>
+	      <li>
+	        <img src="imagenes/slider4.jpg"> <!-- random image -->
+	        <div class="caption left-align">
+	          <h3>UMSS</h3>
+	          <h5 class="light grey-text text-lighten-3">FCyT</h5>
+	        </div>
+	      </li>
+	    </ul>
+  </div>
+      
 
 </article>
 
-</body>
+
 
 
 <footer>
+	<article id="footer">
+		<h3>
+		 <center>
 
-
-
-
-<article id="footer">
-<hr>
- <center>
-
- <h3 id="titulo_footer">Paginas Amigas</br>
- <a id="link_footer" href=""> Fcyt </a>
- <a id="link_footer" href=""> Umss </a>
- <a id="link_footer" href=""> Saga </a>
- <a id="link_footer" href=""> Moodle </a>
- <a id="link_footer" href=""> Caroline </a>
- </h3>
- 
-
- </center>
-
-
-</article>
-
-
-
+			 <h3 id="titulo_footer ">Paginas Amigas</br>
+			 <a id="link_footer" href=""> Fcyt </a>
+			 <a id="link_footer" href=""> Umss </a>
+			 <a id="link_footer" href=""> Saga </a>
+			 <a id="link_footer" href=""> Moodle </a>
+			 <a id="link_footer" href=""> Caroline </a>
+		 </h3>
+	 	</center>
+	</article>
 </footer>
+
+</body>
 </html>
